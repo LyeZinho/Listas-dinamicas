81648ad0bed6bc67d3a10a202bf4a4f1056a73dc
Pedro Kaleb! <>
2021-05-14 11:25:14 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct aluno{
+  char nome[31];
+  char turma[3];
+  float notas;
+  struct aluno *px;
+};
+
+typedef struct aluno ALUNOS;
+
+ALUNOS *aux;
+ALUNOS *seg;
+ALUNOS *px;
+ALUNOS *inicio;
+
+
+int main(void){
+
+  //😎👌Primeira celula
+  aux = ( ALUNOS* ) malloc(sizeof(ALUNOS));
+  //
+  inicio = aux;
+  //Dados
+  strcpy(aux -> nome, "Pedro");
+  strcpy(aux -> turma, "10E");
+  aux -> notas = 19.9;
+  //Proxima celula
+  aux -> px = seg;
+
+
+  //😎👌Segunda celula
+  aux = ( ALUNOS * ) malloc(sizeof(ALUNOS));
+  strcpy(aux -> nome, "Arnaldo");
+  strcpy(aux -> turma, "10E");
+  aux -> notas = 19.9;
+  seg -> px = ;
+
+
+  //😎👌Segunda celula
+  aux = ( ALUNOS * ) malloc(sizeof(ALUNOS));
+  strcpy(aux -> nome, "Algusto");
+  strcpy(aux -> turma, "10E");
+  aux -> notas = 19.9;
+  seg -> px = seg;
+
+//😎👌Segunda celula
+  aux = ( ALUNOS * ) malloc(sizeof(ALUNOS));
+  strcpy(aux -> nome, "Arnaldo");
+  strcpy(aux -> turma, "10E");
+  aux -> notas = 19.9;
+  seg -> px = NULL;
+
+  printf("Criado com sucesso!");
+  return 0;
+}
